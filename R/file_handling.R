e25788ab4ba9300ef318eb78b077112f8a8c5a74
Adam H. Sparks <adamhsparks@gmail.com>
2016-10-28 16:14:58 +1000

    Major rewrite of package structure
    Remove foreach %do% loops, use ply::ldply instead
    
    remove many extra functions from get_GSOD.R and put into files for file
    handling, file_handling.R; validation, validation_functions.R; and file
    processing, process_files.R
    
    Add plyr to list of depends

---
+++ R/file_handling.R
@@ -0,0 +1,94 @@
+# Functions used in GSODR for handling files -----------------------------------
+
+#'@noRd
+.dl_global_files <- function(agroclimatology, country, s, stations, td, tf) {
+
+  tryCatch(Map(function(ftp, dest)
+    utils::download.file(url = ftp, destfile = dest, mode = "wb"),
+    s, tf), error = function(x) message(paste0(
+      "\nThe file downloads have failed. Please restart.\n")))
+
+  message("\nFinished downloading file.
+          \nParsing the indivdual station files now.\n")
+  filelist <- list.files(td, pattern = "^.*\\.gz$", full.names = TRUE)
+
+  Map(function(file, dest) utils::untar(tarfile = filelist, exdir  = dest),
+      file , paste0(file.path(td, substr(filelist, 6, 10))))
+
+  GSOD_list <- list.files(paste0(td, "/", substr(filelist, 6, 10), "/"),
+                          pattern = "^.*\\.op.gz$", full.names = FALSE)
+  # If agroclimatology == TRUE, subset list of stations to process--------------
+  if (agroclimatology == TRUE) {
+    station_list <- stations[stations$LAT >= -60 &
+                               stations$LAT <= 60, ]$STNID
+    station_list <- vapply(station_list,
+                           function(x) rep(paste0(x, "-",
+                                                  substr(filelist, 6, 10),
+                                                  ".op.gz")),
+                           "")
+    GSOD_list <- GSOD_list[GSOD_list %in% station_list == TRUE]
+    rm(station_list)
+  }
+
+  # If country is set, subset list of stations to process ----------------------
+  if (!is.null(country)) {
+    country_FIPS <- unlist(as.character(stats::na.omit(
+      GSODR::country_list[GSODR::country_list$FIPS == country, ][[1]]),
+      use.names = FALSE))
+    station_list <- stations[stations$CTRY == country_FIPS, ]$STNID
+    station_list <- vapply(station_list,
+                           function(x) rep(paste0(x, "-",
+                                                  substr(filelist, 6, 10),
+                                                  ".op.gz")),
+                           "")
+    GSOD_list <- GSOD_list[GSOD_list %in% station_list == TRUE]
+  }
+  GSOD_XY <- plyr::ldply(.data = GSOD_list, .fun = .process_gz)
+  return(GSOD_XY)
+}
+
+
+#' @noRd
+.dl_specified_stations <- function(s, stations, td, years) {
+  filenames <- paste0(substr(s, 1, 43),
+                      strsplit(RCurl::getURL(substr(s, 1, 43),
+                                             ftp.use.epsv = FALSE,
+                                             ftplistonly = TRUE, crlf = TRUE),
+                               "\r*\n")[[1]])[-c(1:2)]
+  s <- filenames[which(s %in% filenames)]
+  tryCatch(Map(function(ftp, dest)
+    utils::download.file(url = ftp, destfile = dest, mode = "wb"),
+    s, paste0(file.path(td, basename(s)))),
+    error = function(x) message(paste0(
+      "\nThe file downloads have failed. Please restart.\n")))
+  GSOD_list <- list.files(td, pattern = "^.*\\.op.gz$", full.names = TRUE)
+  GSOD_XY <- plyr::ldply(.data = GSOD_list, .fun = .process_gz)
+  return(GSOD_XY)
+}
+
+#' @noRd
+.fetch_station_list <- function(){
+  STNID <- NULL
+  stations <- readr::read_csv(
+    "ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.csv",
+    col_types = "ccccccddddd",
+    col_names = c("USAF", "WBAN", "STN_NAME", "CTRY", "STATE", "CALL",
+                  "LAT", "LON", "ELEV_M", "BEGIN", "END"), skip = 1)
+
+  stations[stations == -999.9] <- NA
+  stations[stations == -999] <- NA
+
+  stations <- stations[stations$LAT != 0 & stations$LON != 0, ]
+  stations <- stations[stations$LAT > -90 & stations$LAT < 90, ]
+  stations <- stations[stations$LON > -180 & stations$LON < 180, ]
+  stations$STNID <- as.character(paste(stations$USAF, stations$WBAN, sep = "-"))
+
+  SRTM_GSOD_elevation <- data.table::setkey(GSODR::SRTM_GSOD_elevation, STNID)
+  data.table::setDT(stations)
+  data.table::setkey(stations, STNID)
+  stations <- stations[SRTM_GSOD_elevation, on = "STNID"]
+
+  stations <- stations[!is.na(stations$LAT), ]
+  stations <- stations[!is.na(stations$LON), ]
+  return(stations)
+}
