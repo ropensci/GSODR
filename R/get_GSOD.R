b19699cfb0cea716e856c80ae645364a5cbc81a5
Adam H. Sparks <adamhsparks@gmail.com>
2017-01-18 14:20:47 +1000

    Improve documentation
    Add `@return` field for the four major functions

--- R/get_GSOD.R
+++ R/get_GSOD.R
@@ -213,6 +213,9 @@
 #' SRTM for the globe Version 4, available from the CGIAR-CSI SRTM 90m Database
 #' \url{http://srtm.csi.cgiar.org}}
 #'
+#' @return A \code{data.frame} object of weather data or a comma-separated value
+#' (CSV) or GeoPackage (GPKG) file saved to local disk.
+#'
 #' @seealso \code{\link{reformat_GSOD}}
 #'
 #' @importFrom dplyr %>%
