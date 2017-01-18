120328a0cd257f8e32b8dd3126d0544fbe7168ac
adamhsparks <adamhsparks@gmail.com>
2017-01-18 20:37:16 +1000

    Fix tests for reformat_GSOD()
    Cleanup and redownload the files to be sure for both tests.

--- tests/testthat/test-reformat_GSOD.R
+++ tests/testthat/test-reformat_GSOD.R
@@ -19,10 +19,20 @@ test_that("reformat_GSOD file_list parameter reformats data properly", {
   expect_equal(nrow(x), 1454)
   expect_equal(ncol(x), 48)
   expect_type(x, "list")
+  unlink(destinations)
 })
 
 test_that("reformat_GSOD dsn parameter reformats data properly", {
   skip_on_cran()
+  
+  ftp_base <- "ftp://ftp.ncdc.noaa.gov/pub/data/gsod/1960/"
+  test_files <- c("066000-99999-1960.op.gz", "066200-99999-1960.op.gz",
+                  "066700-99999-1960.op.gz", "066800-99999-1960.op.gz")
+  dest <- tempdir()
+  destinations <- paste0(dest, "/", test_files)
+  
+  Map(function(u, d) download.file(u, d, mode = "wb"),
+      paste0(ftp_base, test_files), destinations)
 
   x <- reformat_GSOD(dsn = dest)
   expect_equal(nrow(x), 1454)
