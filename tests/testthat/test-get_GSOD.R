16e9495281d8bc5618722f55a1b0c9d1de69c514
Adam H. Sparks <adamhsparks@gmail.com>
2017-01-28 08:52:49 +1000

    Add test for validating filename
    In case user does not specify filename, GSODR should default to “GSOD”
    for a filename.

--- tests/testthat/test-get_GSOD.R
+++ tests/testthat/test-get_GSOD.R
@@ -5,7 +5,7 @@ test_that(".validate_years handles invalid years", {
   expect_error(.validate_years(years = NULL),
                "\nYou must provide at least one year of data to download in a numeric\n         format.\n")
   expect_error(.validate_years(years = "nineteen ninety two"),
-                "\nYou must provide at least one year of data to download in a numeric\n         format.\n")
+               "\nYou must provide at least one year of data to download in a numeric\n         format.\n")
   expect_error(.validate_years(years = 1923),
                "\nThe GSOD data files start at 1929, you have entered a year prior\n             to 1929.\n")
   expect_error(.validate_years(years = 1901 + as.POSIXlt(Sys.Date())$year),
@@ -29,22 +29,28 @@ test_that("invalid stations are handled", {
                "\naaa-bbbbbb is not a valid station ID number, please check\n      your entry. Station IDs are provided as a part of the GSODR package in the\n      'stations' data\nin the STNID column.\n")
 })
 
+# Check that GSOD filename is assigned if a user does not specify a name
+test_that("GSOD filename is used when user does not specify a filename", {
+  expect_equal(.validate_fileout(CSV = TRUE, dsn = "~/", filename =  NULL,
+                                 GPKG = NULL), "~/GSOD")
+})
+
 # Check that invalid dsn is handled --------------------------------------------
 test_that("Missing or invalid dsn is handled", {
   dsn <- "~/NULL"
   expect_error(
     if (!is.null(dsn)) {
-    .validate_fileout(CSV = FALSE, dsn = dsn, filename = NULL,
-                                 GPKG = FALSE)
+      .validate_fileout(CSV = FALSE, dsn = dsn, filename = NULL,
+                        GPKG = FALSE)
     },
-               "\nFile dsn does not exist: ~/NULL.\n")
+    "\nFile dsn does not exist: ~/NULL.\n")
 
   expect_error(
     if (!is.null(dsn)) {
-    .validate_fileout(CSV = FALSE, dsn = dsn, filename = "test",
-                                 GPKG = FALSE)
-      },
-               "\nYou need to specify a filetype, CSV or GPKG.")
+      .validate_fileout(CSV = FALSE, dsn = dsn, filename = "test",
+                        GPKG = FALSE)
+    },
+    "\nYou need to specify a filetype, CSV or GPKG.")
   rm(dsn)
 })
 
@@ -65,7 +71,7 @@ test_that("missing days check allows stations with permissible days missing,
             df_list <- list(just_right_2015, too_short_2015)
 
             filenames <- c("just_right_2015", "too_short_2015")
-            sapply(seq_along(df_list),
+            sapply(1:length(df_list),
                    function(x) write.csv(df_list[[x]],
                                          file = gzfile(
                                            paste0(td, "/", filenames[x],
@@ -84,7 +90,7 @@ test_that("missing days check allows stations with permissible days missing,
             expect_length(GSOD_list, 2)
             expect_match(basename(GSOD_list_filtered), "just_right_2015.csv.gz")
             unlink(td)
-          })
+            })
 
 # Check missing days in leap years ---------------------------------------------
 test_that("missing days check allows stations with permissible days missing,
@@ -97,7 +103,7 @@ test_that("missing days check allows stations with permissible days missing,
             df_list <- list(just_right_2016, too_short_2016)
 
             filenames <- c("just_right_2016", "too_short_2016")
-            sapply(seq_along(df_list),
+            sapply(1:length(df_list),
                    function(x) write.csv(df_list[[x]],
                                          file = gzfile(
                                            paste0(td, "/", filenames[x],
@@ -116,21 +122,21 @@ test_that("missing days check allows stations with permissible days missing,
             expect_length(GSOD_list, 2)
             expect_match(basename(GSOD_list_filtered), "just_right_2016.csv.gz")
             unlink(td)
-          })
+            })
 
 # Check that max_missing only accepts positive values --------------------------
 test_that("The 'max_missing' parameter will not accept NA values", {
 
   expect_error(get_GSOD(years = 2010, max_missing = NA),
-"\nThe 'max_missing' parameter must be a positive value larger than
-           1\n")
+               "\nThe 'max_missing' parameter must be a positive value larger than
+               1\n")
 })
 
 test_that("The 'max_missing' parameter will not accept values < 1", {
 
   expect_error(get_GSOD(years = 2010, max_missing = 0.1),
-  "\nThe 'max_missing' parameter must be a positive value larger than
-           1\n")
+               "\nThe 'max_missing' parameter must be a positive value larger than
+               1\n")
 })
 
 # Check validate country returns a two letter code -----------------------------
