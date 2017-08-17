9ec79774c6859a070964f56f79764883aa8b7d91
adamhsparks <adamhsparks@gmail.com>
2017-08-18 06:34:36 +1000

    Clean up messages while running and add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -7,3 +7,17 @@
       utils::globalVariables(c("."))
     }
   }
+
+
+.onAttach <- function(libname, pkgname) {
+  msg <- paste0("\nGSOD is distributed free by the US NCEI with the\n",
+                "following conditions.\n",
+                "'The following data and products may have conditions placed\n",
+                "their international commercial use. They can be used within\n",
+                "the U.S. or for non-commercial international activities\n",
+                "without restriction. The non-U.S. data cannot be\n",
+                "redistributed for commercial purposes. Re-distribution of\n",
+                "these data by others must provide this same notification.\n",
+                "WMO Resolution 40. NOAA Policy'\n")
+  packageStartupMessage(msg)
+}
