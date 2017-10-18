b350b607cd8a2bd329066f9dd6ea91865b79b8ab
Adam H. Sparks <adamhsparks@gmail.com>
2017-10-18 13:55:44 +1000

    Update ROxygen code in get_GSOD()
    Returns a tibble and/or GPKG or CSV

--- R/get_GSOD.R
+++ R/get_GSOD.R
@@ -111,7 +111,7 @@
 #' Hole-filled SRTM for the globe Version 4, available from the CGIAR-CSI SRTM
 #' 90m Database \url{http://srtm.csi.cgiar.org}}
 #'
-#' @return A \code{\link[base]{data.frame}} object of weather data or a
+#' @return A \code{\link[tibble]{tibble}} object of weather data and/or a
 #' comma-separated value (CSV) or GeoPackage (GPKG) file saved to local disk.
 #'
 #' @seealso \code{\link{reformat_GSOD}}
