f2a4932aaa0463f6a1188bb2aba4f9013cc0e980
Adam H. Sparks <adamhsparks@gmail.com>
2017-10-18 10:12:22 +1000

    Fix test for get_inventory() and add new tests
    Now checks col types as well

---
+++ tests/testthat/test-get_inventory.R
@@ -0,0 +1,23 @@
+
+# Check that get_inventory functions properly ----------------------------------
+context("get_inventory()")
+test_that("get_inventory fetches the inventory doc and returns a data frame", {
+  skip_on_cran()
+  x <- get_inventory()
+  expect_length(x, 14)
+  expect_is(x, "data.frame")
+  expect_is(x$STNID, "character")
+  expect_is(x$YEAR, "integer")
+  expect_is(x$JAN, "integer")
+  expect_is(x$FEB, "integer")
+  expect_is(x$MAR, "integer")
+  expect_is(x$APR, "integer")
+  expect_is(x$MAY, "integer")
+  expect_is(x$JUN, "integer")
+  expect_is(x$JUL, "integer")
+  expect_is(x$AUG, "integer")
+  expect_is(x$SEP, "integer")
+  expect_is(x$OCT, "integer")
+  expect_is(x$NOV, "integer")
+  expect_is(x$DEC, "integer")
+})
