e983289178fa5f343727ee05292891cb53600dd0
Adam H. Sparks <adamhsparks@gmail.com>
2017-10-24 16:31:34 +1000

    Make changes to test reformat_GSOD that I originally intended

--- tests/testthat/test-reformat_GSOD.R
+++ tests/testthat/test-reformat_GSOD.R
@@ -1,4 +1,5 @@
 
+
 # Check that reformat_GSOD functions properly ----------------------------------
 context("reformat_GSOD")
 file.remove(file.path(tempdir(), list.files(tempdir(), pattern = ".op.gz$")))
@@ -6,20 +7,39 @@ test_that("reformat_GSOD file_list parameter reformats data properly", {
   skip_on_cran()
 
   ftp_base <- "ftp://ftp.ncdc.noaa.gov/pub/data/gsod/1960/"
-  test_files <- c("066000-99999-1960.op.gz", "066200-99999-1960.op.gz")
-  dest <- tempdir()
-  destinations <- file.path(dest, test_files)
+  test_files <-
+    c("066000-99999-1960.op.gz", "066200-99999-1960.op.gz")
+  dsn <- tempdir()
+  destinations <- file.path(dsn, test_files)
 
-  Map(function(u, d) download.file(u, d, mode = "wb"),
-      paste0(ftp_base, test_files), destinations)
+  Map(
+    function(u, d)
+      download.file(u, d, mode = "wb"),
+    paste0(ftp_base, test_files),
+    destinations
+  )
 
-  file_list <- list.files(dest, pattern = ".op.gz$", full.names = TRUE)
+  file_list <- list.files(path = dsn,
+                          pattern = "^.*\\.op.gz$",
+                          full.names = TRUE)
   expect_equal(length(file_list), 2)
-  expect_equal(basename(file_list), c("066000-99999-1960.op.gz",
-                                      "066200-99999-1960.op.gz"))
+  expect_equal(basename(file_list),
+               c("066000-99999-1960.op.gz",
+                 "066200-99999-1960.op.gz"))
+
+  # check that provided a file list, the function works properly
   x <- reformat_GSOD(file_list = file_list)
   expect_equal(nrow(x), 722)
   expect_length(x, 48)
   expect_is(x, "data.frame")
+
+  # check that provided a dsn only, the function works properly
+  x <- reformat_GSOD(dsn = dsn)
+  expect_equal(nrow(x), 722)
+  expect_length(x, 48)
+  expect_is(x, "data.frame")
+
   unlink(destinations)
+
+
 })
