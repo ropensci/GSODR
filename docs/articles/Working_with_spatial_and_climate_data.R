529dfa85d2c987f20fff44a94f64bd7306a90dee
Adam H. Sparks <adamhsparks@gmail.com>
2018-05-08 14:46:50 +1000

    Update static docs

--- docs/articles/Working_with_spatial_and_climate_data.R
+++
@@ -1,6 +0,0 @@
-## ----check_packages, echo=FALSE, messages=FALSE, warning=FALSE-----------
-required <- c("spacetime", "plotKML", "GSODRdata", "reshape2")
-
-if (!all(unlist(lapply(required, function(pkg) requireNamespace(pkg, quietly = TRUE)))))
-  knitr::opts_chunk$set(eval = FALSE, collapse = TRUE, comment = "#>", fig.width = 7, fig.height = 7, fig.align = "center")
-
