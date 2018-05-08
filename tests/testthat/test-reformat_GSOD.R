bc6c9af94b96a7fdd0ec9de6659221a0d4a41ae5
Adam H. Sparks <adamhsparks@gmail.com>
2018-05-08 14:29:17 +1000

    Fix errors with testing reformat_GSOD()

--- tests/testthat/test-reformat_GSOD.R
+++ tests/testthat/test-reformat_GSOD.R
@@ -1,27 +1,18 @@
 
 # Check that reformat_GSOD functions properly ----------------------------------
 context("reformat_GSOD")
-file.remove(file.path(tempdir(), list.files(tempdir(), pattern = ".op.gz$")))
 test_that("reformat_GSOD file_list parameter reformats data properly", {
   skip_on_cran()
-
   do.call(file.remove, list(list.files(
     tempdir(),
     pattern = ".gz$",
     full.names = TRUE
   )))
 
-  # set up options for .download_files function
-  years <- 1960
-  station <-  c("066000-99999", "066200-99999")
-  cache_dir <- tempdir()
-  ftp_base <- "ftp://ftp.ncdc.noaa.gov/pub/data/gsod/"
-
-
+  # set up options for curl
   dir_list_handle <-
     curl::new_handle(
       ftp_use_epsv = FALSE,
-      dirlistonly = TRUE,
       crlf = TRUE,
       ssl_verifypeer = TRUE,
       ftp_response_timeout = 30,
@@ -29,22 +20,21 @@ test_that("reformat_GSOD file_list parameter reformats data properly", {
     )
 
   ftp_base <- "ftp://ftp.ncdc.noaa.gov/pub/data/gsod/1960/"
-
   test_files <-
     c("066000-99999-1960.op.gz", "066200-99999-1960.op.gz")
-
-  dsn <- tempdir()
-
-  destinations <- file.path(dsn, test_files)
+  destinations <- file.path(tempdir(), test_files)
 
   Map(
     function(u, d)
-      curl_download(u, d, handle = dir_list_handle, mode = "wb"),
+      curl::curl_download(u, d,
+                          handle = dir_list_handle,
+                          mode = "wb",
+                          quiet = TRUE),
     paste0(ftp_base, test_files),
     destinations
   )
 
-  file_list <- list.files(path = cache_dir,
+  file_list <- list.files(path = tempdir(),
                           pattern = "^.*\\.op.gz$",
                           full.names = TRUE)
   expect_equal(length(file_list), 2)
@@ -59,7 +49,7 @@ test_that("reformat_GSOD file_list parameter reformats data properly", {
   expect_is(x, "data.frame")
 
   # check that provided a dsn only, the function works properly
-  x <- reformat_GSOD(dsn = dsn)
+  x <- reformat_GSOD(dsn = tempdir())
   expect_equal(nrow(x), 722)
   expect_length(x, 48)
   expect_is(x, "data.frame")
