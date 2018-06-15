1a9c8f360587f3df64aed09494273f376cdc3f26
Adam H Sparks <adamhsparks@gmail.com>
2018-06-15 15:27:54 +1000

    Remove deprecated functions

--- tests/testthat/test-deprecated.R
+++
@@ -1,7 +0,0 @@
-
-context("get_station_list")
-# Check that the deprecated function returns a message -------------------------
-
-test_that("get_station_list returns a deprecated message", {
-  expect_warning(get_station_list())
-})
