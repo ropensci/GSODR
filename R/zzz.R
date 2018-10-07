00b095327a506d8a98761b9fdbf7327c7110d5a5
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-08 07:45:02 +1000

    Removes startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,4 @@
 
 .onAttach <- function(libname, pkgname) {
-  msg <- paste0("\nGSOD is distributed free by the U.S. NCEI with the\n",
-                "following conditions.\n",
-                "'The following data and products may have conditions placed\n",
-                "their international commercial use. They can be used within\n",
-                "the U.S. or for non-commercial international activities\n",
-                "without restriction. The non-U.S. data cannot be\n",
-                "redistributed for commercial purposes. Re-distribution of\n",
-                "these data by others must provide this same notification.\n",
-                "WMO Resolution 40. NOAA Policy'\n",
-                "\n",
-                "GSODR does not redistribute any weather data itself. It \n",
-                "only provides an interface for R users to download these\n",
-                "data, however it does redistribute station metadata in the\n",
-                "package.\n")
-  packageStartupMessage(msg)
   options(GSODR_connection = stdin())
 }
