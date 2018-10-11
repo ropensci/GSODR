c88eca9bc20917d62109caed60ba5ba661d27888
Adam H. Sparks <adamhsparks@gmail.com>
2018-10-11 13:45:11 +1000

    Update documentation
    - Fixes bug in `reformat_GSOD()` example
    
    - Shortens time required to run example for `get_GSOD()`

--- R/get_GSOD.R
+++ R/get_GSOD.R
@@ -73,14 +73,7 @@
 #' # Download weather station for Toowoomba, Queensland for 2010
 #' t <- get_GSOD(years = 2010, station = "955510-99999")
 #'
-#' # Download data for Philippines for year 2010 with a maximum of five missing
-#' # days per station allowed.
-#'
-#' get_GSOD(years = 2010, country = "Philippines", max_missing = 5)
-#'
-#' # Download global GSOD data for agroclimatology work for years 2010 and 2011
-#'
-#' get_GSOD(years = 2010:2011, agroclimatology = TRUE)
+#' t
 #' }
 #'
 #' @author Adam H Sparks, \email{adamhsparks@@gmail.com}
