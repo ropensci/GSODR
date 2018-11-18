e01a3e27f668ecb6b805df33b0534f8f8eaf0f74
Adam Sparks <adamhsparks@gmail.com>
2018-11-18 20:59:32 +0000

    Update documentation with parallel processing example

--- R/get_GSOD.R
+++ R/get_GSOD.R
@@ -74,7 +74,12 @@
 #' tbar <- get_GSOD(years = 2010, station = "955510-99999")
 #'
 #' tbar
+#' 
+#' # Get agroclimatology data for 2015 using parallel processing
+#' future::plan(multisession)
+#' global <- get_GSOD(years = 2015, agroclimatology = TRUE)
 #'
+#' global
 #' }
 #'
 #' @author Adam H Sparks, \email{adamhsparks@@gmail.com}
