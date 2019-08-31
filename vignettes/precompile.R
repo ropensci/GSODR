7c602005389e6e666a658e9002461ec29ea1747b
Adam H. Sparks <adamhsparks@gmail.com>
2019-09-01 08:52:42 +1000

    Fix bug that prevented 3rd vignette from knitting

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -4,8 +4,8 @@ library(knitr)
 knit("vignettes/GSODR.Rmd.orig", "vignettes/GSODR.Rmd")
 knit("vignettes/Spatial.Rmd.orig", "vignettes/Spatial.Rmd")
 knit(
-  "Working_with_spatial_and_climate_data.Rmd.orig",
-  "Working_with_spatial_and_climate_data.Rmd"
+  "vignettes/Working_with_spatial_and_climate_data.Rmd.orig",
+  "vignettes/Working_with_spatial_and_climate_data.Rmd"
 )
 
 library(devtools)
