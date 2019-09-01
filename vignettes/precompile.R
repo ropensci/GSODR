c6aa33060dcb39425f1bd569d5e71c441c0276e7
Adam H. Sparks <adamhsparks@gmail.com>
2019-09-01 10:53:30 +1000

    Setup new build process for vignettes requiring Internet access

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,5 +1,4 @@
-# vignettes that depend on internet access have been precompiled:
-
+# vignettes that depend on internet access need to be precompiled
 library(knitr)
 knit("vignettes/GSODR.Rmd.orig", "vignettes/GSODR.Rmd")
 knit("vignettes/Spatial.Rmd.orig", "vignettes/Spatial.Rmd")
@@ -8,5 +7,13 @@ knit(
   "vignettes/Working_with_spatial_and_climate_data.Rmd"
 )
 
+# remove file path such that vignette will build with figures
+replace <- readLines("vignettes/GSODR.Rmd")
+replace <- gsub("<img src=\"vignettes/", "<img src=\"", replace)
+fileConn <- file("vignettes/GSODR.Rmd")
+writeLines(replace, fileConn)
+close(fileConn)
+
+# build vignettes
 library(devtools)
 build_vignettes()
