d399e66a12684b26cdfa018d70aad3a1fbba61e6
Adam H. Sparks <adamhsparks@gmail.com>
2019-09-01 19:51:40 +1000

    Clean up precompile script

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -7,13 +7,31 @@ knit(
   "vignettes/Working_with_spatial_and_climate_data.Rmd"
 )
 
-# remove file path such that vignette will build with figures
+# remove file path such that vignettes will build with figures
 replace <- readLines("vignettes/GSODR.Rmd")
 replace <- gsub("<img src=\"vignettes/", "<img src=\"", replace)
 fileConn <- file("vignettes/GSODR.Rmd")
 writeLines(replace, fileConn)
 close(fileConn)
 
+replace <-
+  readLines("vignettes/Working_with_spatial_and_climate_data.Rmd")
+replace <-
+  gsub("vignettes/",
+       "",
+       replace)
+fileConn <-
+  file("vignettes/Working_with_spatial_and_climate_data.Rmd")
+writeLines(replace, fileConn)
+close(fileConn)
+
 # build vignettes
 library(devtools)
 build_vignettes()
+
+# move resource files to /doc
+resources <- list.files("vignettes/", pattern = ".png$", full.names = TRUE)
+file.copy(from = resources, to = "doc", overwrite =  TRUE)
+
+# clean up stray files
+unlink("vignettes/Temperatures_PHL_2010-2010.kmz")
