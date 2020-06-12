9525c4d4c6753d7d31ba8653754ca72f50fe92f3
Adam H. Sparks <adamhsparks@gmail.com>
2020-06-12 22:20:30 +1000

    Remove skimr and Spatial vignettes to slim Suggests section

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,11 +1,6 @@
-# vignettes that depend on internet access need to be precompiled
-library(knitr)
+# vignettes that depend on internet access need to be pre-compiled
+library("knitr")
 knit("vignettes/GSODR.Rmd.orig", "vignettes/GSODR.Rmd")
-knit("vignettes/Spatial.Rmd.orig", "vignettes/Spatial.Rmd")
-knit(
-  "vignettes/Working_with_spatial_and_climate_data.Rmd.orig",
-  "vignettes/Working_with_spatial_and_climate_data.Rmd"
-)
 
 # remove file path such that vignettes will build with figures
 replace <- readLines("vignettes/GSODR.Rmd")
@@ -14,19 +9,8 @@ fileConn <- file("vignettes/GSODR.Rmd")
 writeLines(replace, fileConn)
 close(fileConn)
 
-replace <-
-  readLines("vignettes/Working_with_spatial_and_climate_data.Rmd")
-replace <-
-  gsub("vignettes/",
-       "",
-       replace)
-fileConn <-
-  file("vignettes/Working_with_spatial_and_climate_data.Rmd")
-writeLines(replace, fileConn)
-close(fileConn)
-
 # build vignettes
-library(devtools)
+library("devtools")
 build_vignettes()
 
 # move resource files to /doc
@@ -35,6 +19,3 @@ resources <-
 file.copy(from = resources,
           to = "doc",
           overwrite =  TRUE)
-
-# clean up stray files
-unlink("vignettes/Temperatures_PHL_2010-2010.kmz")
