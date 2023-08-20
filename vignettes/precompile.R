cafe03a8acdf5bce28ab3b85361ae1f9bd4cca39
Adam H. Sparks <adamhsparks@icloud.com>
2023-08-20 19:51:56 +0800

    use snake_case for names

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -5,9 +5,9 @@ knit("vignettes/GSODR.Rmd.orig", "vignettes/GSODR.Rmd")
 # remove file path such that vignettes will build with figures
 replace <- readLines("vignettes/GSODR.Rmd")
 replace <- gsub("<img src=\"vignettes/", "<img src=\"", replace)
-fileConn <- file("vignettes/GSODR.Rmd")
-writeLines(replace, fileConn)
-close(fileConn)
+file_conn <- file("vignettes/GSODR.Rmd")
+writeLines(replace, file_conn)
+close(file_conn)
 
 # build vignettes
 library("devtools")
