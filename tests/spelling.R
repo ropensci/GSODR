ec9b79ccbf265635f80c478f3a90009c0d32f9db
Adam H. Sparks <adamhsparks@icloud.com>
2023-08-20 19:55:42 +0800

    Fix bug in spelling.R

--- tests/spelling.R
+++ tests/spelling.R
@@ -1,3 +1,3 @@
 if (requireNamespace("spelling", quietly = TRUE))
   spelling::spell_check_test(vignettes = TRUE, error = FALSE,
-                             skip_if_offline = TRUE)
+                             skip_on_cran = TRUE)
