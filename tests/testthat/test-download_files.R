f1c288ddd91dd52c3828e4112f3120072b7deabb
Adam H. Sparks <adamhsparks@icloud.com>
2024-07-21 09:04:30 +0800

    Check and update tests for download_files()

--- tests/testthat/test-download_files.R
+++ tests/testthat/test-download_files.R
@@ -1,30 +1,23 @@
+do.call(file.remove, list(list.files(
+  tempdir(), pattern = ".csv$", full.names = TRUE
+)))
+
 test_that(".download_files properly works, subsetting for country and
   agroclimatology works",
-  {
-    skip_if_offline()
-    do.call(file.remove, list(list.files(
-      tempdir(),
-      pattern = ".csv$",
-      full.names = TRUE
-    )))
-    years <- 1982
-    agroclimatology <- TRUE
-    country <- "RP"
-    station <- NULL
+          {
+            skip_if_offline()
+            years <- 1982
+            agroclimatology <- TRUE
+            country <- "RP"
+            station <- NULL
 
-    load(system.file("extdata", "isd_history.rda", package = "GSODR"))
-    setkey(isd_history, "STNID")
-    stations <- isd_history
+            stations <- get_isd_history()
+            setkey(stations, "STNID")
 
-    GSOD_list <- .download_files(station,
-                                 years)
+            GSOD_list <- .download_files(station, years)
 
-    agro_list <- .agroclimatology_list(GSOD_list,
-                                       stations,
-                                       years)
+            agro_list <- .agroclimatology_list(GSOD_list, stations, years)
 
-    RP_list <- .subset_country_list(country,
-                                    GSOD_list,
-                                    stations,
-                                    years)
-  })
+            RP_list <- .subset_country_list(country, GSOD_list, stations, years)
+          })
+
