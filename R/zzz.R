69caa3c206c8d1715ff28a29a4bded7fedf02db8
Adam H. Sparks <adam.sparks@curtin.edu.au>
2026-02-26 14:00:02 +0800

    add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,11 @@
 .onAttach <- function(libname, pkgname) {
   options(GSODR_connection = stdin())
+
+  msg <- paste(
+    "{.strong The GSOD dataset was retired} on {.date 2025-08-29}.",
+    "{.pkg {pkgname}} will not receive further updates.",
+    sep = "\n"
+  )
+
+  packageStartupMessage(cli::format_inline(msg))
 }
